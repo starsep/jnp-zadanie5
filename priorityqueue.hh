659ff1394e14fb4cee9b1aaeeaef4b1b1b5e30d0
Filip Czaplicki <fc359081@students.mimuw.edu.pl>
2016-01-03 17:53:55 +0100

    Complexity/exception safety comments added

--- priorityqueue.hh
+++ priorityqueue.hh
@@ -19,106 +19,123 @@ template<typename K, typename V>
 class PriorityQueue {
 private:
 	std::multiset <std::pair<V, K>> container;
-
-	const std::pair <V, K> &min() const {
-		if (empty()) {
-			throw PriorityQueueEmptyException();
-		}
-		return *container.begin();
-	};
-
-	const std::pair <V, K> &max() const {
-		if (empty()) {
-			throw PriorityQueueEmptyException();
-		}
-		return *container.rbegin();
-	};
 public:
 	using size_type = std::size_t;
 	using key_type = K;
 	using value_type = V;
 
-	// Konstruktor bezparametrowy tworzący pustą kolejkę [O(1)]
+	/*Konstruktor bezparametrowy tworzący pustą kolejkę
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	PriorityQueue() = default;
 
-	// Konstruktor kopiujący [O(queue.size())]
+	/*Konstruktor kopiujący
+	  Złożoność: O(queue.size())
+	  Exception safety: no-throw */
 	PriorityQueue(const PriorityQueue<K, V> &queue)
 		: container(queue.container) {
 	}
 
-	// Konstruktor przenoszący [O(1)]
+	/*Konstruktor przenoszący
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	PriorityQueue(PriorityQueue<K, V> &&queue)
 		: container(std::move(queue.container)) {
 	}
 
-	// Operator przypisania [O(queue.size()) dla użycia P = Q, a O(1) dla użycia
-	// P = move(Q)]
+	/*Operator przypisania dla użycia P = Q
+	  Złożoność: O(queue.size())
+	  Exception safety: strong */
 	PriorityQueue<K, V> &operator=(const PriorityQueue<K, V> &queue) {
 		decltype(container) copy(container);
 		copy = queue.container;
-		std::swap(container, copy);
+		swap(container, copy);
 		return *this;
 	}
 
+	/*Operator przypisania dla użycia P = move(Q)
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	PriorityQueue<K, V> &operator=(PriorityQueue<K, V> &&queue) {
 		container = std::move(queue.container);
 		return *this;
 	}
 
-	// Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta [O(1)]
+	/*Metoda zwracająca true wtedy i tylko wtedy, gdy kolejka jest pusta
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	bool empty() const {
 		return container.empty();
 	}
 
-	// Metoda zwracająca liczbę par (klucz, wartość) przechowywanych w kolejce
-	// [O(1)]
+	/*Metoda zwracająca liczbę par (klucz, wartość) przechowywanych w kolejce
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	size_type size() const {
 		return container.size();
 	}
 
-	// Metoda wstawiająca do kolejki parę o kluczu key i wartości value
-	// [O(log size())] (dopuszczamy możliwość występowania w kolejce wielu
-	// par o tym samym kluczu)
+	/*Metoda wstawiająca do kolejki parę o kluczu key i wartości value
+	  Złożoność: O(log size())
+	  Exception safety: strong*/
 	void insert(const K &key, const V &value) {
-		//TODO
 		container.insert(std::make_pair(value, key));
 	}
 
-	// Metody zwracające odpowiednio najmniejszą i największą wartość przechowywaną
-	// w kolejce [O(1)]; w przypadku wywołania którejś z tych metod na pustej
-	// strukturze powinien zostać zgłoszony wyjątek PriorityQueueEmptyException
+	/*Metoda zwracająca najmniejszą wartość przechowywaną w kolejce
+	  Złożoność: O(1)
+	  Exception safety: strong */
 	const V &minValue() const {
-		return min().first;
+		if (empty()) {
+			throw PriorityQueueEmptyException();
+		}
+		return container.begin()->first;
 	}
 
+	/*Metoda zwracająca największą wartość przechowywaną w kolejce
+	  Złożoność: O(1)
+	  Exception safety: strong */
 	const V &maxValue() const {
-		return max().first;
+		if (empty()) {
+			throw PriorityQueueEmptyException();
+		}
+		return container.rbegin()->first;
 	}
 
-	// Metody zwracające klucz o przypisanej odpowiednio najmniejszej lub
-	// największej wartości [O(1)]; w przypadku wywołania którejś z tych metod
-	// na pustej strukturze powinien zostać zgłoszony wyjątek
-	// PriorityQueueEmptyException
+	/*Metoda zwracająca klucz o przypisanej najmniejszej wartości
+	  Złożoność: O(1)
+	  Exception safety: strong */
 	const K &minKey() const {
-		return min().second;
+		if (empty()) {
+			throw PriorityQueueEmptyException();
+		}
+		return container.begin()->second;
 	}
 
+	/*Metoda zwracająca klucz o przypisanej największej wartości
+	  Złożoność: O(1)
+	  Exception safety: strong */
 	const K &maxKey() const {
-		return max().second;
+		if (empty()) {
+			throw PriorityQueueEmptyException();
+		}
+		return container.rbegin()->second;
 	}
 
-	// Metody usuwające z kolejki jedną parę o odpowiednio najmniejszej lub
-	// największej wartości [O(log size())]
+	/*Metoda usuwająca z kolejki jedną parę o najmniejszej wartości
+	  Złożoność: O(log size())
+	  Exception safety: no-throw */
 	void deleteMin() {
 		if (empty()) {
 			return;
 		}
-		//TODO
 		container.erase(container.begin());
 	}
 
+	/*Metoda usuwająca z kolejki jedną parę o największej wartości
+	  Złożoność: O(log size())
+	  Exception safety: no-throw */
 	void deleteMax() {
-		//TODO
 		if (empty()) {
 			return;
 		}
@@ -127,12 +144,11 @@ public:
 		container.erase(it);
 	}
 
-	// Metoda zmieniająca dotychczasową wartość przypisaną kluczowi key na nową
-	// wartość value [O(log size())]; w przypadku gdy w kolejce nie ma pary
-	// o kluczu key, powinien zostać zgłoszony wyjątek
-	// PriorityQueueNotFoundException(); w przypadku kiedy w kolejce jest kilka par
-	// o kluczu key, zmienia wartość w dowolnie wybranej parze o podanym kluczu
-	void changeValue(const K &key, const V &value) throw() {
+	/*Metoda zmieniająca dotychczasową wartość przypisaną kluczowi key na nową wartość value
+	  Złożoność: O(log size())
+	  Exception safety: strong */
+	void changeValue(const K &key, const V &value) {
+		//TODO
 		std::pair <V, K> pair(value, key);
 		auto it = container.upper_bound(pair);
 		if (it == container.begin()) {
@@ -147,62 +163,75 @@ public:
 		insert(key, value);
 	}
 
-	// Metoda scalająca zawartość kolejki z podaną kolejką queue; ta operacja usuwa
-	// wszystkie elementy z kolejki queue i wstawia je do kolejki *this
-	// [O(size() + queue.size() * log (queue.size() + size()))]
+	/*Metoda scalająca zawartość kolejki z podaną kolejką queue; ta operacja usuwa
+	  wszystkie elementy z kolejki queue i wstawia je do kolejki *this
+	  Złożoność: O(size() + queue.size() * log (queue.size() + size()))
+	  Exception safety: strong */
 	void merge(PriorityQueue<K, V> &queue) {
-		//TODO
 		while (!queue.empty()) {
 			container.insert(*queue.container.begin());
 			queue.container.erase(queue.container.begin());
 		}
 	}
 
-	// Metoda zamieniającą zawartość kolejki z podaną kolejką queue (tak jak
-	// większość kontenerów w bibliotece standardowej) [O(1)]
+	/*Metoda zamieniającą zawartość kolejki z podaną kolejką queue (tak jak
+      większość kontenerów w bibliotece standardowej)
+	  Złożoność: O(1)
+	  Exception safety: no-throw */
 	void swap(PriorityQueue<K, V> &queue) {
-		//TODO
-		std::swap(container, queue.container);
+		container.swap(queue.container);
 	}
 
-	/*Klasa PriorityQueue<K, V> powinna zawierać publiczne definicje typów
-		reprezentujących rozmiar oraz przechowywane klucze i wartości, o nazwach
-		odpowiednio: size_type, key_type, value_type.
-
-	Ponadto powinny być możliwe globalne operacje:
-
-		- swap: przyjmujący dwie kolejki i zamieniający ich zawartość miejscami,
-	- Porównania: ==, !=,
-	- Porównania leksykograficzne kolejek <, >, <=, >=.*/
+	/*Operator porównania
+	  Złożoność: O(size())
+	  Exception safety: strong */
 	bool operator==(const PriorityQueue &queue) const {
 		return container == queue.container;
 	}
 
+	/*Operator porównania
+	  Złożoność: O(size())
+	  Exception safety: strong */
 	bool operator<(const PriorityQueue &queue) const {
 		return container < queue.container;
 	}
 };
 
+/*Operator porównania
+  Złożoność: O(size())
+  Exception safety: strong */
 template<typename K, typename V>
 bool operator!=(const PriorityQueue<K, V> &first, const PriorityQueue<K, V> &second) {
 	return !(first == second);
 }
 
+/*Operator porównania
+  Złożoność: O(size())
+  Exception safety: strong */
 template<typename K, typename V>
 bool operator>(const PriorityQueue<K, V> &first, const PriorityQueue<K, V> &second) {
-	return !(first <= second);
+	return second < first;
 }
 
+/*Operator porównania
+  Złożoność: O(size())
+  Exception safety: strong */
 template<typename K, typename V>
 bool operator>=(const PriorityQueue<K, V> &first, const PriorityQueue<K, V> &second) {
 	return !(first < second);
 }
 
+/*Operator porównania
+  Złożoność: O(size())
+  Exception safety: strong */
 template<typename K, typename V>
 bool operator<=(const PriorityQueue<K, V> &first, const PriorityQueue<K, V> &second) {
-	return first < second || first == second;
+	return !(second < first);
 }
 
+/*Funkcja zamieniającą zawartość dwóch kolejek
+  Złożoność: O(1)
+  Exception safety: no-throw */
 template<typename K, typename V>
 void swap(PriorityQueue<K, V> &first, PriorityQueue<K, V> &second) {
 	first.swap(second);
